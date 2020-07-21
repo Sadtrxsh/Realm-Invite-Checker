8c6c143a6760c95286452daa8683db71b0eebd5a
Sadtrxsh <65015376+Sadtrxsh@users.noreply.github.com>
2020-07-21 14:50:38 -0500

    Update check.py

--- check.py
+++ check.py
@@ -1,5 +1,10 @@
 from colorama import Fore, Style, init
 import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+
 init()
 #SHUT THE FUCK UP ABOUT COLORAMA NOT WORKING, IT FUCKING WORKS OK!!!???
 print(Fore.RED + Style.BRIGHT, """ 
@@ -34,15 +39,20 @@ print(Fore.RED + Style.BRIGHT, """
             ================================
                           
 """)  
-#welcome to this bullshit but in python
+#IF YOU DONT EAT MY ASS I WILL KILL YOU 
+
 def checkRealm(code):
 
     URL = f'https://open.minecraft.net/pocket/realms/invite/{code}'
-    result = requests.post(URL, headers={'Use invite code: '}).text
-    if 'Open Minecraft' in result:
-        return True
-    else: 
-        return False
+    RESPONSE = request.get(URL)
+    soup = BeautifulSoup(response.text, 'html.parser')
+for one_a_tag in soup.findAll('var'):
+	if line_count >= 75:
+		return True
+	else: 
+		return False
+
+line_count +=1
 
 VALID_CODES_FILE   = "valid_realm_codes.txt"
 INVALID_CODES_FILE = "invalid_realm_codes.txt"
@@ -65,4 +75,3 @@ with open(CODES_FILE , 'r') as codes_file:
             invalidTokens_file.write(code + "\n")
 print('Done, now go griefing.')
 input() #the end
-
