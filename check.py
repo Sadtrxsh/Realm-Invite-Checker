67a4c4326f72aecacaf72b0707138eb554b6cce4
Sadtrxsh <65015376+Sadtrxsh@users.noreply.github.com>
2020-07-23 10:49:09 -0500

    Update check.py

--- check.py
+++ check.py
@@ -1,8 +1,6 @@
-from colorama import Fore, Style, init
-import requests
 import urllib.request
-import time
-from bs4 import BeautifulSoup
+import random
+from colorama import Fore, Style, init
 
 
 init()
@@ -39,28 +37,34 @@ print(Fore.RED + Style.BRIGHT, """
             ================================
                           
 """)  
-#IF YOU DONT EAT MY ASS I WILL KILL YOU 
+print(Fore.GREEN + Style.BRIGHT, """YOUR CHANCES OF THIS WORKING IS 
 
+62 quintrillion 50 quadrillion 8 trillion 388 billion 552 million 823 thousand 
+487 possible realm codes
+THATS 3125 out of 32318025202371264 valid for under 6 million realm codes""")
+x=0
 def checkRealm(code):
-
-    URL = f'https://open.minecraft.net/pocket/realms/invite/{code}'
-    RESPONSE = request.get(URL)
-    soup = BeautifulSoup(response.text, 'html.parser')
-for one_a_tag in soup.findAll('var'):
-	if line_count >= 75:
-		return True
-	else: 
-		return False
-
-line_count +=1
-
+    for x in range(0,100000000000000):
+        try:
+            r=urllib.request.urlopen(URL)
+            sourcecode=r.readlines()
+            baa=sourcecode[74]
+            print(Fore.GREEN + Style.BRIGHT, baa)
+            return true 
+            break
+        except:
+            URL = f'https://open.minecraft.net/pocket/realms/invite/{code}'
+            x=x+1
+            print(Fore.RED + Style.BRIGHT, "valid URL not found, attempt "+str(x))
+            
+        
+        
 VALID_CODES_FILE   = "valid_realm_codes.txt"
 INVALID_CODES_FILE = "invalid_realm_codes.txt"
 CODES_FILE         = "realm_codes.txt"
 
 validTokens_file   = open(VALID_CODES_FILE, 'a+')
 invalidTokens_file = open(INVALID_CODES_FILE, 'a+')
-
 with open(CODES_FILE , 'r') as codes_file:
     content = codes_file.readlines()
     codes = list(filter(None, list(map(lambda x: str.replace(x, "\n", ""), content)))) # deletes the useless shit
@@ -74,4 +78,3 @@ with open(CODES_FILE , 'r') as codes_file:
             print(f'{Fore.RED + Style.BRIGHT} {code}', Fore.WHITE + Style.NORMAL)
             invalidTokens_file.write(code + "\n")
 print('Done, now go griefing.')
-input() #the end
